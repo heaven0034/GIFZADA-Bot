727b3b6502d197aff091aaaa9c7f38007d0573f5
heaven <94477820+heaven0034@users.noreply.github.com>
2023-06-05 17:33:59 +0200

    Update ajuda.js

--- commands/utility/ajuda.js
+++ commands/utility/ajuda.js
@@ -5,10 +5,10 @@ const emojis = require('../../emojis.json');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ajuda')
-        .setDescription('✅ Exibe a lista de comandos'),
+        .setDescription('Exibe a lista de comandos'),
     execute(interaction) {
         const mainEmbed = new MessageEmbed()
-            .setColor('#68d19e')
+            .setColor('#c7c7c7')
             .setDescription(`Clique no botão de sua escolha para exibir a página de ajuda correspondente.`)
             .setFooter(interaction.client.user.username, interaction.client.user.displayAvatarURL())
             .setTimestamp();
@@ -32,24 +32,24 @@ module.exports = {
 
         collector.on('collect', (i) => {
             const replyEmbed = new MessageEmbed()
-                .setColor('#68d19e')
+                .setColor('#c7c7c7')
                 .setTitle('Comandos disponíveis 📝')
                 .setDescription(`Aqui está a lista de comandos disponíveis.`)
                 .addFields(
-                    { name: `🌐 Todos - (4)`, value: `${emojis.dot} </help:1114881414299668531> *Exibe a lista de comandos.*\n${emojis.dot} </invite:1114883740074774589> *Cria um convite para o servidor.*\n${emojis.dot} </payment:1114883740074774592> *Exibe opções de pagamento.*\n${emojis.dot} </suggest:1114883740074774594> *Envia uma sugestão para melhoria.*\n${emojis.dot} </verify:1114883740074774595> *Verifique-se.*` },
+                    { name: `🌐 Todos - (3)`, value: `${emojis.dot} </ajuda:1115259724665983009> *Exibe a lista de comandos.*\n${emojis.dot} </avatar:1114997718364999801> *Obtenha o avatar de um usuário.*\n${emojis.dot} </banner:1114998253130362970> *Obtenha o banner de um usuário.*` },
                 )
                 .setFooter(interaction.client.user.username, interaction.client.user.displayAvatarURL())
                 .setTimestamp();
 
             if (interaction.member.permissions.has('MANAGE_ROLES') && interaction.member.permissions.has('MANAGE_SERVER') && interaction.member.permissions.has('MANAGE_MESSAGES')) {
                 replyEmbed.addFields(
-                    { name: `⭐ Cliente - (1)`, value: `${emojis.dot} </vouch:1114910378829299782> *Dê sua opinião sobre o serviço que você adquiriu.*` },
+                    { name: `🔧 Moderator - (4)`, value: `${emojis.dot} </listar:1115001284190277755> *Listar todos os membros com um cargo específico.*\n${emojis.dot} </maker:1115004244030935073> *Ver as notas de um membro.*\n${emojis.dot} </nota-adicional:1115084442025857075> *Adiciona uma nota sobre um usuário.*\n${emojis.dot} </postadores:1115020428138774659> *Ver o ranking dos postadores de imagens.*` },
                 );
             }
 
             if (interaction.member.permissions.has('ADMINISTRATOR')) {
                 replyEmbed.addFields(
-                    { name: `🔨 Administrador - (2)`, value: `${emojis.dot} </mass-role:1114883740074774590> *Adicionar ou remover um cargo de todos os usuários.*\n${emojis.dot} </nuke:1114883740074774591> *Destruir um canal.*` }
+                    { name: `🔨 Administrador - (9)`, value: `${emojis.dot} </adicionar-usario:1115259724665983006> *Adiciona um usuário recrutado.*\n${emojis.dot} </configurar-recrutados:1115078271747510292> *Configura os canais para recrutados.*\n${emojis.dot} </criar-webhook:1115259724665983007> *Criar um webhook.*\n${emojis.dot} </recrutadores:1115076030391128067> *Exibe o ranking dos recrutadores do servidor.*\n${emojis.dot} </recrutados:1115069847391449098> *Exibe os recrutados de um membro específico.*\n${emojis.dot} </remove-recrutador:1115259724665983008> *Remove os dados de um recrutador.*\n${emojis.dot} </reset-recrutadores:1115281726973952002> *Redefine todos os dados dos recrutadores.*\n${emojis.dot} </verificar:1115081892593676289> *Verifica um usuario.*` }
                 );
             }
 
